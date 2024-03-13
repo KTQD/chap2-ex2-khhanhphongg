5e5fc51ec8e0959dfc1d68c13315fcf5530eaca2
khhanhphongg <163244116+khhanhphongg@users.noreply.github.com>
2024-03-13 15:08:02 +0700

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int height, width, perimeter;
+
+    printf("Enter the height and width of the rectangle: ");
+    scanf("%d %d", &height, &width);
+    
+perimeter = 2.0 * (height + width);
+
+   
+    printf("Perimeter of the rectangle: %d\n", perimeter);
+
+    return 0;
+}
